81d9aaf69795cdc996548b41ccd5031084c42955
augustorib <augustorifra@gmail.com>
2022-06-07 00:04:53 -0300

    Aula06 Iniciando configuracao do Panel

--- src/containers/Main/components/Board.jsx
+++ src/containers/Main/components/Board.jsx
@@ -12,7 +12,19 @@ function Board({data}){
     return(
         <Grid container spacing={4}>
             <Grid item xs={12} md={3}>
-                <Card value={getValue(cases)} label="Total de Casos:" color="#5d78ff"/>
+                <Card value={getValue(cases)} label="Total de Casos" color="#5d78ff"/>
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <Card value={getValue(todayDeaths)} label="Óbitos hoje" color="#F7B829" />
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <Card value={getValue(todayCases)} label="Casos hoje" color="#000" />
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <Card value={getValue(deaths)} label="Total de mortos" color="#E95078" />
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <Card value={getValue(recovered)} label="Total de recuperados" color="#67C887" />
             </Grid>
         </Grid>
     )
